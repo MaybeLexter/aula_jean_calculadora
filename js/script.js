4a834dc5808b57249f7842acb342bd1b6e96a95e
Leonardi <109309175+MaybeLexter@users.noreply.github.com>
2024-06-14 20:01:58 -0400

    arrumando2

--- js/script.js
+++ js/script.js
@@ -3,24 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
   const buttons = Array.from(document.getElementsByClassName('button'));
 
   buttons.forEach(button => {
-    button.addEventListener('click', function() {
-      const value = this.getAttribute('data-value');
+      button.addEventListener('click', function() {
+          const value = this.getAttribute('data-value');
 
-      if (value === 'C') {
-        display.innerText = '0';
-      } else if (value === '=') {
-        try {
-          display.innerText = eval(display.innerText);
-        } catch {
-          display.innerText = 'Error';
-        }
-      } else {
-        if (display.innerText === '0' || display.innerText === 'Error') {
-          display.innerText = value;
-        } else {
-          display.innerText += value;
-        }
-      }
-    });
+          if (value === 'C') {
+              display.innerText = '0';
+          } else if (value === '=') {
+              try {
+                  display.innerText = eval(display.innerText);
+              } catch {
+                  display.innerText = 'Error';
+              }
+          } else {
+              if (display.innerText === '0' || display.innerText === 'Error') {
+                  display.innerText = value;
+              } else {
+                  display.innerText += value;
+              }
+          }
+      });
   });
 });
